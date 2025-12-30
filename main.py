3013e6174d8013fd316289709fc7201f75c6d2f7
import-sys <import.sys.dev@gmail.com>
2025-12-30 17:35:47 +0200

    feat: abstract gamepy from the player code

--- main.py
+++ main.py
@@ -2,28 +2,28 @@ import pygame
 from pytmx.util_pygame import load_pygame
 
 
-def game_loop(screen, player_sprite):
-
-    # SHOW THE PLAYER
-    screen.blit(player_sprite, (720, 450))    
-
+def game_loop():
+    ### ENTER YOUR CODE HERE ###
+    draw_player(720, 450)
 
 
 ### DO NOT EDIT BELOW THIS LINE YET ###
+
+pygame.init()
+player_sprite = pygame.image.load("assets/character.png")
+pygame.display.set_caption("Python adventure")
+screen = pygame.display.set_mode((800, 640), pygame.RESIZABLE | pygame.SCALED)
+
 def make_game():
-    pygame.init()
-    pygame.display.set_caption("Python adventure")
-    screen = pygame.display.set_mode((800, 640), pygame.RESIZABLE | pygame.SCALED)
     clock = pygame.time.Clock()
     running = True
     game_map = load_pygame("assets/map/map.tmx")
-    player_sprite = pygame.image.load("assets/character.png")
     while running:
         for event in pygame.event.get():
             if event.type == pygame.QUIT:
                 running = False
         draw_map(game_map, screen)
-        game_loop(screen, player_sprite)
+        game_loop()
 
         pygame.display.flip()
         clock.tick(60)
@@ -35,6 +35,33 @@ def draw_map(game_map, screen):
         for x,y,sprite in layer.tiles():
             screen.blit(sprite, (x * sprite.get_width(), y * sprite.get_height()))
 
+
+def draw_player(x, y):
+    screen.blit(player_sprite, (x, y))
+
+def draw_text(text, x, y, color):
+    font = pygame.font.SysFont("Arial", 8)
+    drawable = font.render(text, False, color)
+    screen.blit(drawable, (x, y))
+
+def draw_rectangle(x, y, w, h, color):
+    pygame.draw.rect(screen, color, (x, y, w, h))
+
+def is_key_pressed(key):
+    keys = pygame.key.get_pressed()
+    return keys[key]
+
+def left_pressed():
+    return is_key_pressed(pygame.K_a)
+
+def right_pressed():
+    return is_key_pressed(pygame.K_d)
+
+def up_pressed():
+    return is_key_pressed(pygame.K_w)
+
+def down_pressed():
+    return is_key_pressed(pygame.K_s)
 
 if __name__ == '__main__':
     make_game()
