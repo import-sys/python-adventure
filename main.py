56b9d51f0d25098b044bb49275529eca5785bd13
import-sys <import.sys.dev@gmail.com>
2026-01-29 09:10:02 +0200

    feat: boundries check

--- main.py
+++ main.py
@@ -5,12 +5,27 @@ player_x = 720
 player_y = 450
 
 
+screen_width = 800
+screen_height = 640
+
+player_width = 16
+player_height = 16
+
+
 def game_loop():
     ### ENTER YOUR CODE HERE ###
 
     global player_x, player_y
 
-    player_x = player_x - 1
+    player_speed_x = 1
+    player_speed_y = 1
+
+
+    if player_x >= 0 and player_x + player_width <= screen_width:
+        player_x = player_x + player_speed_x
+
+    if player_y >= 0 and player_y + player_height <= screen_height:
+        player_y = player_y + player_speed_y
 
     draw_player(player_x, player_y)
 
