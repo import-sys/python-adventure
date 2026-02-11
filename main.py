9fb165b5f5b91563f7ebf057d1e4de1a89ac2bc2
import-sys <import.sys.dev@gmail.com>
2026-02-11 13:59:31 +0200

    feat: using dicts as NPC and player data container

--- main.py
+++ main.py
@@ -1,56 +1,55 @@
 import pygame
 from pytmx.util_pygame import load_pygame
 
-player_x = 720
-player_y = 450
-
-
 screen_width = 800
 screen_height = 640
 
-player_width = 16
-player_height = 16
+player = {
+    "x": 720,
+    "y": 450,
+    "speed_x": 0,
+    "speed_y": 0,
+    "size": 16
+}
 
 npcs = [
-    ["Alice", 305, 405],
-    ["Bob", 160, 375],
-    ["John", 450, 250]
+    {"name": "Alice", "x": 305, "y": 405},
+    {"name": "Bob", "x": 160, "y": 375},
+    {"name": "John", "x": 450, "y": 250},
 ]
 
 
 def game_loop():
     ### ENTER YOUR CODE HERE ###
 
-    global player_x, player_y
-
-    player_speed_x = 0
-    player_speed_y = 0
+    player["speed_x"] = 0
+    player["speed_y"] = 0
 
     if left_pressed():
-        player_speed_x = -1
+        player["speed_x"] = -1
     elif right_pressed():
-        player_speed_x = 1
+        player["speed_x"] = 1
 
     if up_pressed():
-        player_speed_y = -1
+        player["speed_y"] = -1
     elif down_pressed():
-        player_speed_y = 1
+        player["speed_y"] = 1
 
-    if player_x >= 0 and player_x + player_width <= screen_width:
-        player_x = player_x + player_speed_x
+    if player["x"] >= 0 and player["x"] + player["size"] <= screen_width:
+        player["x"] = player["x"] + player["speed_x"]
     else:
         print("Going offscreen")
 
-    if player_y >= 0 and player_y + player_height <= screen_height:
-        player_y = player_y + player_speed_y
+    if player["y"] >= 0 and player["y"] + player["size"] <= screen_height:
+        player["y"] = player["y"] + player["speed_y"]
     else:
         print("Going offscreen")
 
-    draw_player(player_x, player_y)
+    draw_player(player["x"], player["y"])
 
     for npc in npcs:
-        draw_rectangle(npc[1], npc[2], 16, 16, "blue")
-        draw_text(npc[0], npc[1], npc[2] - 10, "red")
+        draw_rectangle(npc["x"], npc["y"], 16, 16, "blue")
+        draw_text(npc["name"], npc["x"], npc["y"] - 10, "red")
 
 
 ### DO NOT EDIT BELOW THIS LINE YET ###
